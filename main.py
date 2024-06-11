93395689d63003ea1787fa9422432fce50a72322
1076060 - Roan <1076060@hr.nl>
2024-06-12 01:15:50 +0200

    ADDED: Lessend API usage

--- main.py
+++ main.py
@@ -53,39 +53,38 @@ def find_active_document(documents):
 rpc = Presence(DISCORD_CLIENT_ID)
 rpc.connect()
 
-# Example usage
-try:
-    user_info = get_user_info()
-    user_name = user_info["name"]
-    user_email = user_info["email"]
-    
-    all_documents = get_all_documents()
-    
-    active_document = find_active_document(all_documents)
-    if active_document:
-        document_name = active_document["name"]
-        document_id = active_document["id"]
-        last_modified = active_document["modifiedAt"]
-        
-        print(f'Active Document ID: {document_id}')
-        print(f'Name: {document_name}')
-        print(f'Last Modified: {last_modified}')
-        
-        # Update Discord Rich Presence with a button linking to the GitHub project
-        rpc.update(
-            state=f"Editing: {document_name}",
-            details=f"User: {user_name}",
-            large_image="onshape_logo",
-            large_text="Onshape CAD",
-            small_image="onshape_logo",  
-            small_text="IIRoan/OnshapeDRPC",
-            buttons=[{"label": "Onshape CAD", "url": "https://cad.onshape.com"}] 
-        )
-    else:
-        print("No active document found.")
-except requests.exceptions.RequestException as e:
-    print(f"An error occurred: {e}")
-
 # Keep the presence updated
 while True:
-    time.sleep(15)  # Discord updates every 15 seconds
+    try:
+        user_info = get_user_info()
+        user_name = user_info["name"]
+        user_email = user_info["email"]
+        
+        all_documents = get_all_documents()
+        
+        active_document = find_active_document(all_documents)
+        if active_document:
+            document_name = active_document["name"]
+            document_id = active_document["id"]
+            last_modified = active_document["modifiedAt"]
+            
+            print(f'Active Document ID: {document_id}')
+            print(f'Name: {document_name}')
+            print(f'Last Modified: {last_modified}')
+            
+            # Update Discord Rich Presence with a button linking to the GitHub project
+            rpc.update(
+                state=f"Editing: {document_name}",
+                details=f"User: {user_name}",
+                large_image="onshape_logo",
+                large_text="Onshape CAD",
+                small_image="onshape_logo",  
+                small_text="IIRoan/OnshapeDRPC",
+                buttons=[{"label": "Onshape CAD", "url": "https://cad.onshape.com"}] 
+            )
+        else:
+            print("No active document found.")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+    
+    time.sleep(30)  # Wait for 30 seconds before the next API query
